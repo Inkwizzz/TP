2ca57153ef41a1289a10a52e580e2a229303838f
Jen_ <113123913+Nerisfiel@users.noreply.github.com>
2023-09-15 12:42:45 +0300

    Add files via upload

---
+++ project1/project1/Form2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project1
+{
+    public partial class Form2 : Form
+    {
+        Users u = new Users();
+        string FN = @"C:\Games\555.txt";
+        public Form2(Users u)
+        {
+            this.u = u;
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ project1/project1/Users.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    public class Users
+    {
+        string user = "";
+        public string get_user()
+        {
+            return user;
+        }
+    }
+}
